306b1922d219f9e38af8a3304e8bb66f232302fe
Angel Vega <andresvegacordero007@gmail.com>
2022-11-04 21:11:11 -0600

    Agregar archivos de proyecto.

---
+++ Monitor.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace Monitor
+{
+    partial class Monitor
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Monitor));
+            this.tabControlMonitoreo = new System.Windows.Forms.TabControl();
+            this.TabPage1 = new System.Windows.Forms.TabPage();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.cmbSites = new System.Windows.Forms.ComboBox();
+            this.txtAddRoom = new System.Windows.Forms.TextBox();
+            this.lblAddRoom = new System.Windows.Forms.Label();
+            this.GrdRooms = new System.Windows.Forms.DataGridView();
+            this.Username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Website = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Autorec = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.tabControlMonitoreo.SuspendLayout();
+            this.TabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GrdRooms)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControlMonitoreo
+            // 
+            this.tabControlMonitoreo.Controls.Add(this.TabPage1);
+            this.tabControlMonitoreo.Location = new System.Drawing.Point(16, 16);
+            this.tabControlMonitoreo.Name = "tabControlMonitoreo";
+            this.tabControlMonitoreo.SelectedIndex = 0;
+            this.tabControlMonitoreo.Size = new System.Drawing.Size(854, 648);
+            this.tabControlMonitoreo.TabIndex = 1;
+            // 
+            // TabPage1
+            // 
+            this.TabPage1.Controls.Add(this.btnRemove);
+            this.TabPage1.Controls.Add(this.btnAdd);
+            this.TabPage1.Controls.Add(this.cmbSites);
+            this.TabPage1.Controls.Add(this.txtAddRoom);
+            this.TabPage1.Controls.Add(this.lblAddRoom);
+            this.TabPage1.Controls.Add(this.GrdRooms);
+            this.TabPage1.Location = new System.Drawing.Point(4, 25);
+            this.TabPage1.Name = "TabPage1";
+            this.TabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.TabPage1.Size = new System.Drawing.Size(846, 619);
+            this.TabPage1.TabIndex = 0;
+            this.TabPage1.Text = "Monitoring";
+            this.TabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnRemove.Location = new System.Drawing.Point(574, 43);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(92, 33);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnAdd.Location = new System.Drawing.Point(459, 43);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(92, 33);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // cmbSites
+            // 
+            this.cmbSites.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSites.FormattingEnabled = true;
+            this.cmbSites.Location = new System.Drawing.Point(266, 48);
+            this.cmbSites.Name = "cmbSites";
+            this.cmbSites.Size = new System.Drawing.Size(174, 24);
+            this.cmbSites.TabIndex = 3;
+            // 
+            // txtAddRoom
+            // 
+            this.txtAddRoom.Location = new System.Drawing.Point(20, 48);
+            this.txtAddRoom.Name = "txtAddRoom";
+            this.txtAddRoom.Size = new System.Drawing.Size(237, 22);
+            this.txtAddRoom.TabIndex = 2;
+            // 
+            // lblAddRoom
+            // 
+            this.lblAddRoom.AutoSize = true;
+            this.lblAddRoom.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblAddRoom.Location = new System.Drawing.Point(17, 16);
+            this.lblAddRoom.Name = "lblAddRoom";
+            this.lblAddRoom.Size = new System.Drawing.Size(240, 17);
+            this.lblAddRoom.TabIndex = 1;
+            this.lblAddRoom.Text = "Add a room or username to monitor :";
+            // 
+            // GrdRooms
+            // 
+            this.GrdRooms.AllowUserToAddRows = false;
+            this.GrdRooms.AllowUserToDeleteRows = false;
+            this.GrdRooms.ColumnHeadersHeight = 30;
+            this.GrdRooms.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.GrdRooms.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Username,
+            this.Website,
+            this.Status,
+            this.Size,
+            this.Autorec});
+            this.GrdRooms.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.GrdRooms.Location = new System.Drawing.Point(20, 95);
+            this.GrdRooms.Name = "GrdRooms";
+            this.GrdRooms.RowHeadersVisible = false;
+            this.GrdRooms.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.GrdRooms.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.GrdRooms.RowTemplate.Height = 24;
+            this.GrdRooms.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.GrdRooms.Size = new System.Drawing.Size(796, 507);
+            this.GrdRooms.TabIndex = 0;
+            // 
+            // Username
+            // 
+            this.Username.HeaderText = "Username";
+            this.Username.MinimumWidth = 6;
+            this.Username.Name = "Username";
+            this.Username.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Username.Width = 125;
+            // 
+            // Website
+            // 
+            this.Website.HeaderText = "Website";
+            this.Website.MinimumWidth = 6;
+            this.Website.Name = "Website";
+            this.Website.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Website.Width = 125;
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.MinimumWidth = 6;
+            this.Status.Name = "Status";
+            this.Status.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Status.Width = 125;
+            // 
+            // Size
+            // 
+            this.Size.HeaderText = "Size";
+            this.Size.MinimumWidth = 6;
+            this.Size.Name = "Size";
+            this.Size.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Size.Width = 125;
+            // 
+            // Autorec
+            // 
+            this.Autorec.HeaderText = "Autorec";
+            this.Autorec.MinimumWidth = 6;
+            this.Autorec.Name = "Autorec";
+            this.Autorec.ReadOnly = true;
+            this.Autorec.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Autorec.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Autorec.Width = 125;
+            // 
+            // Monitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1404, 689);
+            this.Controls.Add(this.tabControlMonitoreo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Monitor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Graba Vacas 3000";
+            this.tabControlMonitoreo.ResumeLayout(false);
+            this.TabPage1.ResumeLayout(false);
+            this.TabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GrdRooms)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControlMonitoreo;
+        private System.Windows.Forms.TabPage TabPage1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ComboBox cmbSites;
+        private System.Windows.Forms.TextBox txtAddRoom;
+        private System.Windows.Forms.Label lblAddRoom;
+        private System.Windows.Forms.DataGridView GrdRooms;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Username;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Website;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Size;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Autorec;
+    }
+}
+
---
+++ Monitor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monitor
+{
+    public partial class Monitor : Form
+    {
+        public List<string> subdominio_stripchat = new List<string>();
+
+        public Monitor()
+        {
+            InitializeComponent();
+            GrdRooms.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+            GrdRooms.AllowUserToResizeColumns = false;
+            GrdRooms.AllowUserToResizeRows = false;
+            fillCmbSites();
+        }
+
+        /// <summary>
+        /// Llena el combobox de los sitios
+        /// </summary>
+        void fillCmbSites()
+        {
+            //Carga los sitios del combobox
+            cmbSites.Items.Add("Chaturbate");
+            cmbSites.Items.Add("Stripchat");
+            cmbSites.Items.Add("Camsoda");
+            cmbSites.Items.Add("Cam4");
+            cmbSites.Items.Add("MyFreeCams");
+
+            //Selecciona la opción por defecto
+            cmbSites.SelectedItem = "Chaturbate";
+        }
+
+        /// <summary>
+        /// Agrega una nueva sala
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ///valida si el campo de la sala esta vacio
+            if (string.IsNullOrEmpty(txtAddRoom.Text))
+            {
+                MessageBox.Show("Enter a valid room or username");
+            }
+            else
+            {
+                //Agrega la sala a la tabla
+                AddRoom();
+            }
+        }
+
+        /// <summary>
+        /// Agrega las salas
+        /// </summary>
+        void AddRoom()
+        {
+            GrdRooms.Columns[0].Name = "Usename";
+            GrdRooms.Columns[1].Name = "Site";
+
+            string[] row1 = new string[] { txtAddRoom.Text, Convert.ToString(cmbSites.SelectedItem) };
+            GrdRooms.Rows.Add(row1);
+
+            //Se limpia el textbox de la sala
+            txtAddRoom.Text = string.Empty;
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                DialogResult dialogResult = MessageBox.Show("Confirm", "Remove Model?", MessageBoxButtons.YesNo);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    GrdRooms.Rows.RemoveAt(GrdRooms.CurrentRow.Index);
+                    MessageBox.Show("Enter a valid room or username");
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+        }
+    }
+}
